3cdffee849ccc1e68f8eb7d88bb8db1b1b2c1f7a
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-08-30 18:38:50 +0300

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,80 @@ import { Loader } from "./Loader/Loader"
 import { Modal } from "./Modal/Modal"
 
 export class App extends Component {
+  state = {
+    query:'',
+    images: [],
+    page:1
+    }
+
+      handleSubmit = (e) =>{
+        e.preventDefault()
+        
+        this.setState({
+          query: e.target.elements.query.value,
+          images: [],
+          page:1
+        })
+        console.log(this.state.images)
+      }
+
+      componentDidUpdate(prevProps, prevState) { 
+        if (prevState.query !== this.state.query || prevState.page !== this.state.page){
+          // HTTP
+          
+          fetch('https://pixabay.com/api/?q=cat&page=1&key=38315175-abb8429954921ba34a6a526ed&image_type=photo&orientation=horizontal&per_page=12')
+      .then(resp => resp.json()).then(cards => this.setState({ images: cards}))
+     
+        }
+      } 
+
+    handleLoaderMore = () => {
+      console.log(this.state.images)
+      this.setState(prevState => ({ page: prevState.page + 1}))
+      console.log(this.state.page)
+    }
   
   render(){
   return (
     <div>
-      <Searchbar/>
-      <Loader/>
-      <ImageGallery/>
+      {/* <Searchbar/> */}
+      <header className="searchbar">
+                    <form
+                     className="form" 
+                    onSubmit={this.handleSubmit}
+                     >
+                    <button type="submit" className="button">
+                    <span className="button-label">Search</span>
+                    </button>
+
+                    <input
+                        className="input"
+                        type="text"
+                        name="query"
+                        // autocomplete="off"
+                        // autofocus
+                       placeholder="Search images and photos"
+                      //  value={this.state.query}
+                      //  onChange={this.handleChange}
+                    />
+                    
+                    </form>
+                </header>
+                {this.state.images.length > 0 && <div>GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG</div>}
+      {/* <Loader/> */}
+      {/* <ImageGallery/> */}
+
+      {this.state.images.length > 0 && <div>XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</div>}
+
+
+
       {/* <Modal/> */}
-      <Button/>
+
+
+      <div>
+        <button type="button" className="button" onClick={this.handleLoaderMore}>Load more</button>
+    </div>
+      {/* <Button/> */}
     </div>
   )}
 };
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,26 +2,31 @@ import { Component } from "react";
 
 export class Searchbar extends Component {
     state = {
-        value: ''
+      query:'',
+      cards: null,
+      page:1
       }
       componentDidMount () {
         fetch('https://pixabay.com/api/?q=cat&page=1&key=38315175-abb8429954921ba34a6a526ed&image_type=photo&orientation=horizontal&per_page=12')
-        .then(resp => resp.json()).then(console.log)
+        .then(resp => resp.json()).then(cards => this.setState({ cards}))
       }
-      handleChange = ({target: {value}}) =>{
-        this.setState({value})
-      }
+      // handleChange = ({target: {value}}) =>{
+      //   this.setState({value})
+      // }
 
-      HandleSubmit = (e) =>{
-        e.preventDefault()
-        console.log(this.state)
-      }
+      // HandleSubmit = (e) =>{
+      //   e.preventDefault()
+      //   console.log(this.state)
+      // }
 
     render() {
         return(
             <div>
                 <header className="searchbar">
-                    <form className="form" onSubmit={this.HandleSubmit}>
+                    <form
+                     className="form" 
+                    //  onSubmit={this.HandleSubmit}
+                     >
                     <button type="submit" className="button">
                     <span className="button-label">Search</span>
                     </button>
@@ -32,11 +37,12 @@ export class Searchbar extends Component {
                         // autocomplete="off"
                         // autofocus
                        placeholder="Search images and photos"
-                       value={this.state.value}
-                       onChange={this.handleChange}
+                      //  value={this.state.value}
+                      //  onChange={this.handleChange}
                     />
                     </form>
                 </header>
+                {/* {this.state.cards && <p>{console.log(this.state.cards.hits[0].largeImageURL)}</p>} */}
             </div>
         )
     }
