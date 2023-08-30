8a89b4eb242f5c773c30748591928347856d5438
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-08-30 12:20:46 +0300

    steps-create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
+import { Component } from "react"
+
+export class App extends Component {
+  render(){
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
     </div>
-  );
+  )}
 };
+
+// style={{
+//   height: '100vh',
+//   display: 'flex',
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   fontSize: 40,
+//   color: '#010101'
+// }}
