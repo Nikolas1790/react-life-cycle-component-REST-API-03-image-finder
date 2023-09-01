0c4d5ac2c60c122556ffa14fc5c48a798222aff0
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2023-09-01 10:09:28 +0300

    try-dell-error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react"
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar } from "./Searchbar/Searchbar"
 import { ImageGallery } from "./ImageGallery/ImageGallery"
